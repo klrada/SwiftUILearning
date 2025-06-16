a595d84b37aad970146fcbe64244e95b35bdc19c
Akhmed Abdulaev <lonely.violet113@gmail.com>
2025-06-16 16:57:38 +0300

    Добавил ветку View with Heart

--- SwiftUILearning/BackgroundAndOverlayPractice.swift
+++ SwiftUILearning/BackgroundAndOverlayPractice.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct BackgroundAndOverlayPractice: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Image(systemName: "heart.fill")
+            .font(.system(size: 40))
+            .foregroundStyle(.white)
+            .background(
+                Circle()
+                    .fill(
+                        LinearGradient(colors: [.purple, .pink], startPoint: .topLeading, endPoint: .bottomTrailing)
+                    )
+                    .frame(width: 100, height: 100)
+                    .shadow(color: .pink.opacity(0.3), radius: 10, x: 5, y: 5)
+                    .overlay(alignment: .bottomTrailing, content: {
+                        Circle()
+                            .fill(Color(uiColor: .systemPurple))
+                            .frame(width: 35, height: 35)
+                            .overlay(
+                                Text("5")
+                                    .font(.headline)
+                                    .foregroundStyle(.white)
+                            )
+                    })
+            )
     }
 }
 
