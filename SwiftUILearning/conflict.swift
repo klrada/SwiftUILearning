4463fd65624d0c98c275d4d674be5b2d5b33c762
Akhmed Abdulaev <lonely.violet113@gmail.com>
2025-06-17 12:36:47 +0300

    Конфликт feature

--- SwiftUILearning/conflict.swift
+++ SwiftUILearning/conflict.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct conflict: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Text("ABC")
-        Text("ABC")
+        Text("CCD")
+        Text("DDS")
     }
 }
 
